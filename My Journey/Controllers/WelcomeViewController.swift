cc1fa633303893d0e42da19ef890c1a93ddbbf55
iosdev <lauri92.riikonen@gmail.com>
2021-04-21 15:41:22 +0300

    Added a line of code

--- My Journey/Controllers/WelcomeViewController.swift
+++ My Journey/Controllers/WelcomeViewController.swift
@@ -13,6 +13,7 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello welcome")
+        print("Another print")
     }
 
 
